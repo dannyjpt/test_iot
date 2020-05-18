505b1033f1c7a5ef7d43426038f9085024ae0b67
dannyjpt <dpuerta27@gmail.com>
2020-05-17 19:14:17 -0500

    Update subscriber.js

--- subscriber.js
+++ subscriber.js
@@ -1,5 +1,5 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect([{ host: 'localhost', port: 3000 }])
+var client  = mqtt.connect([{ host: 'ws://iotechome.herokuapp.com', port: 3000 }])
 
 client.on('connect', function () {
   	client.subscribe('outTopic', function (err) {
